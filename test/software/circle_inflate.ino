41f38321fd25c1ef6c9df5f2e5fa30850f75cef1
Moshe Sherman <mtshrmn@gmail.com>
2024-02-21 13:48:41 +0200

    add timestamp to circle_inflate

--- test/software/circle_inflate.ino
+++ test/software/circle_inflate.ino
@@ -3,11 +3,15 @@
 #include <Firebase_ESP_Client.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
+#include "time.h"
+#include <NTPClient.h>
+
 
 #define API_KEY "AIzaSyABl_UJRO3JkxAhHKfxys7xxDQHMSz4exg"
 #define DB_URL "https://mirrored-object-default-rtdb.europe-west1.firebasedatabase.app"
 #define SSID "meirl"
 #define PSK "metoothanks"
+#define NTP_SERVER "pool.ntp.org"
 
 
 
@@ -24,28 +28,36 @@ int radius = 0;
 int remoteRadius = 0;
 int buttonState = 0;
 bool isPressed = false;
+char buff[20];
 
-void setup() {
-  pinMode(23, INPUT_PULLUP);
 
-  u8g2.begin();
-  u8g2.setDrawColor(1);
-  u8g2.setFont(u8g2_font_ncenB08_tr);
+char *getTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    return "err";
+  }
+  strftime(buff, 19, "%H:%M:%S", &timeinfo);
+  return buff;
+}
 
+
+void init_wifi() {
   Serial.begin(9600);
   WiFi.begin(SSID, PSK);
   delay(1000);
+  Serial.print("Connected with IP: ");
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(300);
   }
   Serial.println();
-  Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
+}
 
-  config.api_key = API_KEY;
+void init_firebase() {
+ config.api_key = API_KEY;
   config.database_url = DB_URL;
 
   if (Firebase.signUp(&config, &auth, "", "")){
@@ -62,6 +74,22 @@ void setup() {
   Firebase.reconnectWiFi(true);
 }
 
+void init_screen() {
+  u8g2.begin();
+  u8g2.setDrawColor(1);
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+
+}
+
+void setup() {
+  pinMode(23, INPUT_PULLUP);
+
+  init_screen();
+  init_wifi();
+  configTime(7200, 7200, NTP_SERVER);
+  init_firebase();
+}
+
 void loop() {
     // firebase synchronization
     if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis == 0)){
@@ -72,7 +100,12 @@ void loop() {
         Serial.println("error reading remote radius");
       }
 
-      if (Firebase.RTDB.setInt(&fbdo, "board_b", radius)) {
+      if (Firebase.RTDB.setInt(&fbdo, "board_a/total_presses", radius)) {
+        // do nothing
+      } else {
+        Serial.println("error setting local radius");
+      }
+      if (Firebase.RTDB.setString(&fbdo, "board_a/last_connected", getTime())) {
         // do nothing
       } else {
         Serial.println("error setting local radius");
@@ -92,7 +125,16 @@ void loop() {
 
     // render monitor
     u8g2.clearBuffer();
-    u8g2.drawCircle(64, 32, remoteRadius % 30);
+    if (WiFi.status() != WL_CONNECTED) {
+      u8g2.drawStr(0, 20, "wifi error");
+    } else {
+      if (!Firebase.ready()) {
+        u8g2.drawStr(0, 40, "fb error");
+      } else {
+        u8g2.drawCircle(64, 32, remoteRadius % 30);
+      }
+    }
+    
     u8g2.sendBuffer();
 }
 
